46b6ce20967be55e326320b161a6e96d3a988d60
Sergey Klunniy <s.klunniy@gmail.com>
2020-02-05 01:20:38 +0200

    delete comment2

--- src/main/java/com/dev/cinema/dao/impl/MovieSessionDaoImpl.java
+++ src/main/java/com/dev/cinema/dao/impl/MovieSessionDaoImpl.java
@@ -50,10 +50,10 @@ public class MovieSessionDaoImpl implements MovieSessionDao {
             session.get(MovieSession.class, movieId);
 
             CriteriaBuilder cb = session.getCriteriaBuilder();
-            CriteriaQuery<MovieSession> cq = cb.createQuery(MovieSession.class);// результат + вказівник
-            Root<MovieSession> root = cq.from(MovieSession.class); //звернення до таблиці
-            Predicate predicateId = cb.equal(root.get("movie"), movieId); // WHERE moviessession.movie = movieId
-            Predicate predicateDate = cb.between(root.get("showTime"),// WHERE moviessession.showTime = showTime
+            CriteriaQuery<MovieSession> cq = cb.createQuery(MovieSession.class);
+            Root<MovieSession> root = cq.from(MovieSession.class);
+            Predicate predicateId = cb.equal(root.get("movie"), movieId);
+            Predicate predicateDate = cb.between(root.get("showTime"),
                     showTime.atStartOfDay(), showTime.plusDays(1).atStartOfDay());
             cq.select(root).where(cb.and(predicateId, predicateDate)); //+
             return session.createQuery(cq).getResultList();
