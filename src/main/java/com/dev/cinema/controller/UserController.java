cca7ca87273fa68fb98347ccf403732569718057
Sergey Klunniy <s.klunniy@gmail.com>
2020-02-19 16:48:55 +0200

    сделал правки еще раз

--- src/main/java/com/dev/cinema/controller/UserController.java
+++ src/main/java/com/dev/cinema/controller/UserController.java
@@ -1,10 +1,8 @@
 package com.dev.cinema.controller;
 
-import com.dev.cinema.dao.UserDao;
 import com.dev.cinema.model.User;
 import com.dev.cinema.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
